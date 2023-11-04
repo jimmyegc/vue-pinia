83fac4ccc9e30995dfd4015116e538d01f5f11b2
Jimmy García <jimmyegc@gmail.com>
2023-11-03 20:55:12 -0600

    first commit

---
+++ src/stores/CartStore.js
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+
+export const useCartStore = defineStore("CartStore", {
+  state: () => {
+    return {
+      items: [],
+    };
+  },
+  getters: {
+    count() {
+      return this.items.length;
+    },
+    isEmpty() {
+      return this.count === 0;
+    },
+  },
+  actions: {
+    addItems(count, item) {
+      count = parseInt(count);
+      for (let index = 0; index < count; index++) {
+        this.items.push({ ...item });
+      }
+    },
+  },
+});
