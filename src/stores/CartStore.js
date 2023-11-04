635e7ed0cb24f4be8059be0bf422a80174eca6f0
Jimmy García <jimmyegc@gmail.com>
2023-11-03 22:57:23 -0600

    fix 11032023

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,6 @@
 import { defineStore } from "pinia";
+import { groupBy } from "lodash";
+import { useAuthUserStore } from "./AuthStore";
 
 export const useCartStore = defineStore("CartStore", {
   state: () => {
@@ -7,19 +9,37 @@ export const useCartStore = defineStore("CartStore", {
     };
   },
   getters: {
-    count() {
-      return this.items.length;
+    count: (state) => state.items.length,
+    isEmpty: (state) => state.count === 0,
+    grouped: (state) => {
+      const grouped = groupBy(state.items, (item) => item.name);
+      const sorted = Object.keys(grouped).sort();
+      let inOrder = {};
+      sorted.forEach((key) => (inOrder[key] = grouped[key]));
+      return inOrder;
     },
-    isEmpty() {
-      return this.count === 0;
-    },
+    groupCount: (state) => (name) => state.grouped[name].length,
+    total: (state) => state.items.reduce((p, c) => p + c.price, 0),
   },
   actions: {
+    checkout() {
+      const userAuthUserStore = useAuthUserStore();
+      alert(
+        `${userAuthUserStore.username} bought ${this.count} items at a total of $ ${this.total}`
+      );
+    },
     addItems(count, item) {
       count = parseInt(count);
       for (let index = 0; index < count; index++) {
         this.items.push({ ...item });
       }
+    },
+    clearItem(itemName) {
+      this.items = this.items.filter((item) => item.name != itemName);
+    },
+    setItemCount(item, count) {
+      this.clearItem(item.name);
+      this.addItems(count, item);
     },
   },
 });
